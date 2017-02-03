b7660500ef98fa1b669fa2e3b08379824db48f18
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-02-03 06:51:55 -0500

    implemented templates for form and list views; updated app.js with relevant logic

--- js/app.js
+++ js/app.js
@@ -33,11 +33,65 @@ var app = (function () {
             el: $('#menu')
           });
         }
-        return this.menuView
+        return this.menuView;
+      },
+
+      list: function () {
+        if (!this.listView) {
+          this.listView = new api.views.list({
+            model: api.todos
+          });
+        }
+        return this.listView;
+      },
+
+      form: function () {
+        if (!this.formView) {
+          this.formView = new api.views.form({
+            model: api.todos
+          }).on('saved', function () {
+            api.router.navigate('', {trigger: true});
+          })
+        }
+        return this.formView;
       }
     },
-    Router = Backbone.Router.extend({}),
-    api.router = new Router();
+    Router = Backbone.Router.extend({
+      routes: {
+        'archive': 'archive',
+        'new': 'newToDo',
+        'edit/:index': 'editToDo',
+        'delete:/index': 'deleteToDo',
+        '': 'list'
+      },
+
+      list: function (archive) {
+        var view = ViewsFactory.list();
+
+        api
+          .title(archive ? 'Archive:' : 'Your ToDos')
+          .changeContent(view.$el);
+          view.setMode(archive ? 'archive' : null).render();
+      },
+      archive: function () {
+        this.list(true);
+      },
+      newToDo: function () {
+        var view = ViewsFactory.form();
+        api.title('Create new ToDo: ').changeContent(view.$el);
+        view.render();
+      },
+      editToDo: function (index) {
+        var view = ViewsFactory.form();
+        api.title('Edit: ').changeContent(view.$el);
+        view.render(index);
+      },
+      deleteToDo: function (index) {
+        api.todos.remove(api.todos.at(parseInt(index)));
+        api.router.navigate('', {trigger: true});
+      }
+    });
+  api.router = new Router();
 
   return api;
 }());
